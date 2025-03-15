fa85bc0d22c88ca2928cb760288affe417a00fe2
t-kuha <imagingtechnerd@gmail.com>
2025-03-15 15:09:09 +0900

    Bug fix

--- build_tangnano.sh
+++ build_tangnano.sh
@@ -8,26 +8,26 @@ source ${TOP_DIR}/exports.sh
 mkdir -p ${LITEX_DIR}
 
 # sources
-# if [ -e ${SRC_DIR}/nextpnr ]; then
-#     rm -rf ${SRC_DIR}/nextpnr
-# fi
-# git clone https://github.com/YosysHQ/nextpnr.git ${SRC_DIR}/nextpnr -b nextpnr-0.7
+if [ -e ${SRC_DIR}/nextpnr ]; then
+    rm -rf ${SRC_DIR}/nextpnr
+fi
+git clone https://github.com/YosysHQ/nextpnr.git ${SRC_DIR}/nextpnr -b nextpnr-0.7
 
 # install necessary Python packages
-# uv pip install apycula meson
-# uv cache clean
+uv pip install apycula meson
+uv cache clean
 
 # ----- build
 # nextpnr
-# rm -rf ${BUILD_DIR}/nextpnr/_build          # remove existing build
-# pushd ${SRC_DIR}/nextpnr > /dev/null
-# patch <${TOP_DIR}/patches/nextpnr.patch     # fix coompilation error
-# popd > /dev/null
-# cmake -S ${SRC_DIR}/nextpnr -B ${BUILD_DIR}/nextpnr/_build \
-# -DCMAKE_PREFIX_PATH=${TOOLS_DIR} -DCMAKE_INSTALL_PREFIX=${TOOLS_DIR} \
-# -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_RPATH=${TOOLS_DIR}/lib -DARCH=gowin
-# cmake --build ${BUILD_DIR}/nextpnr/_build -- -j${NCPU}
-# cmake --install ${BUILD_DIR}/nextpnr/_build
+rm -rf ${BUILD_DIR}/nextpnr/_build          # remove existing build
+pushd ${SRC_DIR}/nextpnr > /dev/null
+patch <${TOP_DIR}/patches/nextpnr.patch     # fix coompilation error
+popd > /dev/null
+cmake -S ${SRC_DIR}/nextpnr -B ${BUILD_DIR}/nextpnr/_build \
+-DCMAKE_PREFIX_PATH=${TOOLS_DIR} -DCMAKE_INSTALL_PREFIX=${TOOLS_DIR} \
+-DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_RPATH=${TOOLS_DIR}/lib -DARCH=gowin
+cmake --build ${BUILD_DIR}/nextpnr/_build -- -j${NCPU}
+cmake --install ${BUILD_DIR}/nextpnr/_build
 
 # LiteX
 pushd ${LITEX_DIR} > /dev/null
@@ -36,5 +36,5 @@ if [ ! -e litex_setup.py ]; then
     chmod +x litex_setup.py 
     patch litex_setup.py <${TOP_DIR}/patches/litex.patch
 fi
-# ./litex_setup.py --init --install
+./litex_setup.py --init --install
 popd > /dev/null
