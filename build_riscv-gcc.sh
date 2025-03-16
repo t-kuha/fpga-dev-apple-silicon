ddcc0b7d6126a8e5e32d15e03ae5b512e065ab5d
t-kuha <imagingtechnerd@gmail.com>
2025-03-16 18:10:14 +0900

    Update to latest riscv-toolchain

--- build_riscv-gcc.sh
+++ build_riscv-gcc.sh
@@ -3,26 +3,18 @@
 # reference: https://msyksphinz.hatenablog.com/entry/2022/05/23/040000
 #
 
-# num. of CPU cores
-NCPU=$(sysctl -n hw.ncpu)
-
 # create temp. directory
 TOP_DIR=$(dirname $(realpath "${BASH_SOURCE[0]}"))
-TOOLS_DIR=${TOP_DIR}/tools
-WORK_DIR=${TOP_DIR}/_work
+source ${TOP_DIR}/exports.sh
 
 if [ ! -e ${TOOLS_DIR} ]; then
     echo "tools directory not found, exitting..."
     exit
 fi
-if [ ! -e ${WORK_DIR} ]; then
-    mkdir -p ${WORK_DIR}
-fi
-
-export PATH=${TOOLS_DIR}/bin:${PATH}
+mkdir -p ${WORK_DIR}
 
 # ----- install Ninja
-pip install ninja
+uv pip install ninja
 
 # ----- gcc
 export RISCV=${TOOLS_DIR}
@@ -30,16 +22,15 @@ export RISCV=${TOOLS_DIR}
 if [ -e ${WORK_DIR}/riscv-gnu-toolchain ]; then
     rm -rf ${WORK_DIR}/riscv-gnu-toolchain
 fi
-git clone https://github.com/riscv-collab/riscv-gnu-toolchain.git ${WORK_DIR}/riscv-gnu-toolchain -b 2023.07.07
-pushd ${TOP_DIR} > /dev/null
-cd ${WORK_DIR}/riscv-gnu-toolchain
+git clone https://github.com/riscv-collab/riscv-gnu-toolchain.git ${WORK_DIR}/riscv-gnu-toolchain -b 2025.01.20
+pushd ${WORK_DIR}/riscv-gnu-toolchain > /dev/null
 git submodule sync
 git submodule update --init --recursive
 
 if [ -e build ]; then
     rm -rf build
 fi
-mkdir build && cd build
-../configure --prefix=${TOOLS_DIR} --enable-multilib
+mkdir build && pushd build > /dev/null
+../configure --prefix=${TOOLS_DIR} --enable-multilib --disable-gdb
 make -j${NCPU}
 popd > /dev/null
